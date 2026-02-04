452774ba18697d8734fb4fd010e7d59bc003ac32
Lotfy12 <lotfymohmed35@gmail.com>
2026-02-04 15:30:08 +0200

    updata-URL-and-KEY-for-supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://olyffotvdeyjgwsxiqic.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9seWZmb3R2ZGV5amd3c3hpcWljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3OTg3NTcsImV4cCI6MjAyMzM3NDc1N30.Q57DCuFmtsPIOYDcHsw1gnb0hiu_yhQx1bXh47NvYNE";
+const supabaseUrl = "https://vzgphlkojwnuthzzrwfw.supabase.co";
+const supabaseKey = "sb_publishable_zvLMwxUka0vCpWUnGTyRPg_D8H8Gqx_";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
