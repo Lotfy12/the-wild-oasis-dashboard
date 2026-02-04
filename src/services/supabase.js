87c2aa32b8021786d3e24a4d7839862e26f06e8d
Lotfy12 <lotfymohmed35@gmail.com>
2026-02-04 15:39:49 +0200

    updata-URL-and-KEY-for-supabase

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,4 +3,5 @@ const supabaseUrl = "https://vzgphlkojwnuthzzrwfw.supabase.co";
 const supabaseKey = "sb_publishable_zvLMwxUka0vCpWUnGTyRPg_D8H8Gqx_";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+
 export default supabase;
